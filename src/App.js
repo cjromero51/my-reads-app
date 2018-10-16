90755931225e27ddf567e4f70348b7028cdd8d65
CJ Romero <cjromero51@yahoo.com>
2018-10-16 11:38:43 -0700

    shelf switching function created

--- src/App.js
+++ src/App.js
@@ -27,6 +27,18 @@ class BooksApp extends React.Component {
       console.log(this.state.books)
     })
   }
+  switchShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(res => {
+      let newBooksArray = this.state.books;
+      const newBooks = newBooksArray.filter(newBook => newBook.id === book.id)
+      if (newBooks[0]) {
+        newBooks[0].shelf = shelf;
+      } else {
+        newBooksArray.push(book)
+      }
+      this.setState({ books: newBooksArray})
+    })
+  }
   render() {
     return (
       <BookCase books={this.state.books} reloadAllBooks={this.reloadAllBooks}/>
