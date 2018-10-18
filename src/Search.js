d774830c258d888b5f6b71bf9cedb6fbcb872c5d
CJ Romero <cjromero51@yahoo.com>
2018-10-18 01:33:39 -0700

    everything works but input field c-count

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import escapeRegExp from 'escape-string-regexp'
+// import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
@@ -8,43 +8,46 @@ import Book from './Book'
 class Search extends React.Component {
   state = {
     query: '',
-    books: [],
-    showSearchPage: true
+    books: []
   }
-  updateQuery = (query) => {
-    this.setState({ query });
-    this.updateSearch();
-  }
-  filteringDuplicates = (arr) => {
-    let filteredBooks = []
-    for (var i = 0; i < arr.length; i++) {
-      if (filteredBooks.indexOf(arr[i]) === -1) {
-        filteredBooks.push(arr[i])
-      }
-    }
-    return filteredBooks
-  }
-  updateSearch = () => {
-    if (this.state.query === '') {
-      this.setState({ books: [] })
-    } else {
-      BooksAPI.search(this.state.query).then( res => {
-        let shownBooks = this.props.allBooks.concat(res)
-        let noDuplicates = this.filteringDuplicates(shownBooks);
-        const matchingText = new RegExp(escapeRegExp(this.state.query), 'i')
 
-        let finalFilter = noDuplicates.filter(book => matchingText.test(book.title) || matchingText.test(book.authors))
-        return finalFilter.sort(sortBy('title'))
-      }).then(res => {
-        this.setState({books: res})
+  updateQuery = (query) => {
+    let inputTest = document.getElementById('myInput')
+    if (inputTest.value.length === 0) {
+      this.setState({query: ''})
+    }
+    this.setState({ query });
+    this.updateSearch(this.state.query);
+  }
+
+  combineBookCaseAndQuery = (currentBooks, query) => {
+    const ht = {};
+    currentBooks.forEach(book => ht[book.id] = book.shelf);
+
+    query.forEach(book => {
+      book.shelf = ht[book.id] || 'none';
+    })
+    return query.sort(sortBy('title'));
+  }
+  updateSearch = (query) => {
+    if (query.value === '') {
+      this.setState({ books: [] })
+      return;
+    } if (query.length !== 0) {
+      BooksAPI.search(this.state.query).then( res => {
+        if(res.length) {
+          let finalFilter = []
+          finalFilter = this.combineBookCaseAndQuery(this.props.allBooks, res)
+          this.setState({books: finalFilter})
+        }
       })
     }
   }
-  // componentWillReceiveProps = (props) => {
-  //   this.props = props;
-  //   let shownBooks = this.props.allBooks.concat(this.state.books)
-  //   this.setState({books: shownBooks})
-  // }
+  componentWillReceiveProps = (props) => {
+    this.props = props;
+    let finalFilter = this.combineBookCaseAndQuery(this.props.allBooks, this.state.books)
+    this.setState({books: finalFilter})
+  }
   render(){
 
 
@@ -54,10 +57,11 @@ class Search extends React.Component {
           <Link className="close-search" to="/">Close</Link>
             <div className="search-books-input-wrapper">
               <input
+                id='myInput'
                 type="text"
                 placeholder="Search by title or author"
-                onChange={(event) => (this.updateQuery(event.target.value))}
                 value={this.state.query.value}
+                onChange={(event) => (this.updateQuery(event.target.value))}
                 />
               </div>
             </div>
